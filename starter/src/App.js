a97a0ba0c71dca243466fc1d756d980a5827c34f
Derrick Marr <dmarr000@yahoo.com>
2021-03-18 17:54:58 -0400

    attempt github pages

--- starter/src/App.js
+++ starter/src/App.js
@@ -34,48 +34,6 @@ function RenderQuiz({questions}) {
   }
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
-  /*const questions = [
-    {
-      // id: 0,
-      questionText: "What is the capital of France?",
-      answerOptions: [
-        { answerText: "New York", isCorrect: false },
-        { answerText: "London", isCorrect: false },
-        { answerText: "Paris", isCorrect: true },
-        { answerText: "Dublin", isCorrect: false },
-      ],
-    },
-    {
-      // id: 1,
-      questionText: "Who is CEO of Tesla?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Elon Musk", isCorrect: true },
-        { answerText: "Bill Gates", isCorrect: false },
-        { answerText: "Tony Stark", isCorrect: false },
-      ],
-    },
-    {
-      // id: 2,
-      questionText: "The iPhone was created by which company?",
-      answerOptions: [
-        { answerText: "Apple", isCorrect: true },
-        { answerText: "Intel", isCorrect: false },
-        { answerText: "Amazon", isCorrect: false },
-        { answerText: "Microsoft", isCorrect: false },
-      ],
-    },
-    {
-      // id: 3,
-      questionText: "How many Harry Potter books are there?",
-      answerOptions: [
-        { answerText: "1", isCorrect: false },
-        { answerText: "4", isCorrect: false },
-        { answerText: "6", isCorrect: false },
-        { answerText: "7", isCorrect: true },
-      ],
-    },
-  ]; */
 
   return (
     <div className="app">
